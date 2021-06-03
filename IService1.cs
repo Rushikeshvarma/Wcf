59800823d4f720af884626eafebebb9099e5148f
Rushikesh Varma Pericherla <rushikeshpericherla1999@gmail.com>
2021-06-03 15:18:12 +0530

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        //[OperationContract]
+        //[WebGet(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        //Customer GetDataUsingDataContract();
+
+        //[OperationContract]
+        //[WebGet(UriTemplate = "CustomersXml", ResponseFormat = WebMessageFormat.Xml, RequestFormat = WebMessageFormat.Xml)]
+        //Customer GetDataUsingDataContractXML();
+
+
+
+
+        [OperationContract]
+        //[WebGet(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [WebInvoke(UriTemplate = "CustomersJson", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        void PostDataUsingDataContract(Customer c);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "CustomersXml", ResponseFormat = WebMessageFormat.Xml, RequestFormat = WebMessageFormat.Xml)]
+        List<Customer> GetDataUsingDataContractXML();
+
+
+    }
+    [DataContract]
+    public class Customer
+    {
+
+
+        //[DataMember]
+        //public string CustName { get; set; }
+
+        
+        public string custname;
+        [DataMember]
+        public string CustName
+        {
+            get { return custname; }
+            set { custname = value; }
+        }
+
+
+
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        //public Customer GetDataUsingDataContract()
+        //{
+        //    Customer c = new Customer();
+        //    c.CustName = "Hii";
+        //    return c;
+
+        //}
+        //public Customer GetDataUsingDataContractXML()
+        //{
+        //    Customer c = new Customer();
+        //    c.CustName = "Hello world";
+        //    return c;
+
+        //}
+        static List<Customer> custlist = new List<Customer>();
+        public List<Customer> GetDataUsingDataContractXML()
+        {
+            //Customer c = new Customer();
+
+            return custlist;
+            //throw new NotImplementedException();
+        }
+
+        public void PostDataUsingDataContract(Customer c)
+        {
+            Customer c1 = new Customer();
+            c1.CustName = c.CustName;
+            custlist.Add(c1);
+            //throw new NotImplementedException();
+        }
+
+
+    }
+}
